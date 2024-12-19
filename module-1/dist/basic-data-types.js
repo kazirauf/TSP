555c5dee905e4d24212e8e52d921ba5e7e6a64e1
kazirauf <raufelahi9876@gmail.com>
2024-12-19 18:25:47 +0600

    data types

---
+++ module-1/dist/basic-data-types.js
@@ -0,0 +1,18 @@
+"use strict";
+// !basic data types
+// *String 
+const CName = 'kazi rauf';
+// *Number
+const age = 25;
+// *Boolean
+const isSuccess = true;
+// *null
+const nullValue = null;
+//*undefined
+const undefinedValue = undefined;
+// *array
+const number = [20, 23, 45, 23, 12, 546];
+const allNames = ["kazi", "jack", "mesi", "tingu"];
+// *tuple
+const person = ["panu", 15];
+console.log(person);
---
+++ module-1/src/basic-data-types.ts
@@ -0,0 +1,34 @@
+// !basic data types
+
+// *String 
+
+const CName: string = 'kazi rauf';
+
+// *Number
+
+const age: number = 25;
+
+// *Boolean
+
+const isSuccess:boolean = true;
+
+// *null
+
+const nullValue:null = null;
+
+//*undefined
+
+const undefinedValue:undefined = undefined;
+
+
+// *array
+
+const number:number[] = [20, 23, 45, 23, 12, 546]; 
+
+const allNames:string[] = ["kazi", "jack", "mesi", "tingu"]
+
+// *tuple
+
+const person:[string, number] = ["panu", 15]
+
+console.log(person)
