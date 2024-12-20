eb480c43675ee865b5e96828ef65f576a543b282
kazirauf <raufelahi9876@gmail.com>
2024-12-20 23:41:51 +0600

    complete block

---
+++ module-1/src/next-code.ts
@@ -0,0 +1,24 @@
+{
+    
+const user: {
+    id: string;
+    company: "DevMaster Guild"; // type ---> literal types
+    name: string;
+    isMarried?: boolean;
+    age: number;
+    phoneNumber: string;
+    address:string
+    email: string;
+   
+    
+   } ={
+   id: "7984bh",
+   company: "DevMaster Guild",
+   name: "kazi rauf elahi",
+   age:13,
+   phoneNumber: "01404555331",
+   address: "rajshahi, bangladesh",
+   email: "raufelahi9876@gmail.com"
+   }
+   
+}
